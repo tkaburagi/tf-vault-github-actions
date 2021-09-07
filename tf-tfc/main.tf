de1a164c8852424419bbb9a144056e2138e213f3
tkaburagi <t.kaburagi@me.com>
2021-09-07 22:23:29 +0900

    tfc

--- tf-tfc/main.tf
+++ tf-tfc/main.tf
@@ -13,28 +13,32 @@ variable "arm_sub_id" {}
 
 resource "tfe_variable" "arm_client_id" {
   key          = "ARM_CLIENT_ID"
-  value        = "my_value_name"
+  value        = var.arm_client_id
   category     = "env"
+  sensitive = true
   workspace_id = var.workspace_id
 }
 
 resource "tfe_variable" "arm_client_secret" {
   key          = "ARM_CLIENT_SECRET"
-  value        = "my_value_name"
+  value        = var.arm_client_secret
   category     = "env"
+  sensitive = true
   workspace_id = var.workspace_id
 }
 
 resource "tfe_variable" "arm_tenant_id" {
   key          = "ARM_TENANT_ID"
-  value        = "my_value_name"
+  value        = var.arm_tenant_id
   category     = "env"
+  sensitive = true
   workspace_id = var.workspace_id
 }
 
 resource "tfe_variable" "arm_sub_id" {
   key          = "ARM_SUBSCRIPTION_ID"
-  value        = "my_value_name"
+  value        = var.arm_sub_id
   category     = "env"
+  sensitive = true
   workspace_id = var.workspace_id
 }
