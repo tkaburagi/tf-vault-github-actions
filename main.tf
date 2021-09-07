34cd4aba591abb2de8840a98fc43bb38e35bf364
tkaburagi <t.kaburagi@me.com>
2021-09-07 22:32:13 +0900

    make file

--- main.tf
+++ main.tf
@@ -3,10 +3,3 @@ terraform {}
 provider "azurerm" {
     features {}
 }
-
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network-se-kabu"
-  resource_group_name = "se-kabu"
-  location            = "Japan East"
-  address_space       = ["10.0.0.0/16"]
-}
