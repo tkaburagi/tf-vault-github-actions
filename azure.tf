63c675330308ec68d27e9eb31213c65a580ef9dd
tkaburagi <t.kaburagi@me.com>
2021-09-18 14:14:54 +0900

    create network

--- azure.tf
+++ azure.tf
@@ -2,5 +2,5 @@ resource "azurerm_virtual_network" "example" {
   name                = "example-network-se-kabu"
   resource_group_name = "se-kabu"
   location            = "Japan East"
-  address_space       = ["10.22.0.0/16"]
+  address_space       = ["10.10.0.0/16"]
 }
