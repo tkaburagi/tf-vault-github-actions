a8f4b567289ef423407d6b99366804e5b21aa632
tkaburagi <t.kaburagi@me.com>
2021-09-20 12:05:52 +0900

    added instance

--- main.tf
+++ main.tf
@@ -3,11 +3,3 @@ terraform {}
 provider "azurerm" {
     features {}
 }
-
-variable "location" {
-    default = "East Asia"
-}
-variable "hello_tf_instance_count" {
-    default = 1
-}
-variable "admin_password" {}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "location" {
+    default = "East Asia"
+}
+variable "hello_tf_instance_count" {
+    default = 1
+}
+variable "admin_password" {}
